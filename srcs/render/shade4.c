87bba84699f539de0784bffadce5f7d5cf9139d5
daekulee <daekulee@gmail.com>
2023-02-25 20:44:31 +0900

    cy_side

--- srcs/render/shade4.c
+++ srcs/render/shade4.c
@@ -6,16 +6,16 @@
 /*   By: daegulee <daegulee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/24 21:11:22 by daegulee          #+#    #+#             */
-/*   Updated: 2023/02/25 20:27:26 by daegulee         ###   ########.fr       */
+/*   Updated: 2023/02/25 20:43:47 by daegulee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "render.h"
 
-void	_uv_pattern_(t_hit_rec *cur_h_rec)
+void	_uv_pattern_(t_hit_rec *cur_h_rec, double check_n)
 {
-	const double	checker_width = 5.0;
-	const double	checker_height = 5.0;
+	const double	checker_width = check_n;
+	const double	checker_height = check_n;
 	const double	on_u = modulo(cur_h_rec->u * checker_width);
 	const double	on_v = modulo(cur_h_rec->v * checker_height);
 
@@ -31,6 +31,7 @@ void	spherical_mapping(t_hit_rec *cur_h_rec)
 
 	cur_h_rec->u = 0.5 + theta / (2 * PI);
 	cur_h_rec->v = 1 - phi / PI;
+	_uv_pattern_(cur_h_rec, 5.0);
 }
 
 // void	planar_mapping(t_hit_rec *cur_h_rec)
@@ -53,6 +54,7 @@ void	planar_mapping(t_hit_rec *cur_h_rec)
 	cur_h_rec->contact_point.z;
 	cur_h_rec->u = fabs(fmod(local_x, 100)) / 100;
 	cur_h_rec->v = fabs(fmod(local_y, 100)) / 100;
+	_uv_pattern_(cur_h_rec, 5.0);
 }
 
 // void	disk_mapping(t_hit_rec *cur_h_rec)
@@ -77,6 +79,7 @@ void	cy_mapping(t_hit_rec *cur_h_rec)
 	const t_mat4	local_cord = _normal_cord_(cur_h_rec->hit_normal);
 	double			local_x;
 	double			local_y;
+	double			local_z;
 	double			theta;
 
 	local_x = local_cord.m[0][0] * cur_h_rec->contact_point.x + \
@@ -85,9 +88,13 @@ void	cy_mapping(t_hit_rec *cur_h_rec)
 	local_y = local_cord.m[1][0] * cur_h_rec->contact_point.x + \
 	local_cord.m[1][1] * cur_h_rec->contact_point.y + local_cord.m[1][2] * \
 	cur_h_rec->contact_point.z;
-	theta = atan2(local_y, local_x);
+	local_z = local_cord.m[2][0] * cur_h_rec->contact_point.x + \
+	local_cord.m[2][1] * cur_h_rec->contact_point.y + local_cord.m[2][2] * \
+	cur_h_rec->contact_point.z;
+	theta = atan2(local_x, local_z);
 	cur_h_rec->u = 0.5 + theta / (2 * PI);
-	cur_h_rec->v = fabs(fmod(local_y, 2)) / 2;
+	cur_h_rec->v = fabs(fmod(local_y, 40)) / 40;
+	_uv_pattern_(cur_h_rec, 12);
 }
 
 void	get_uv(t_hit_rec *cur_h_rec)
@@ -102,5 +109,4 @@ void	get_uv(t_hit_rec *cur_h_rec)
 		cy_mapping(cur_h_rec);
 	else
 		return ;
-	_uv_pattern_(cur_h_rec);
 }
