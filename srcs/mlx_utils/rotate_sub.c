7ead4f5591382899ff0394bbeb56a179128bea06
이대규 <idaegyu@idaegyuui-MacBookAir.local>
2023-03-06 04:45:48 +0900

    really finish refactoring

--- srcs/mlx_utils/rotate_sub.c
+++ srcs/mlx_utils/rotate_sub.c
@@ -6,7 +6,7 @@
 /*   By: idaegyu <idaegyu@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/06 04:15:06 by idaegyu           #+#    #+#             */
-/*   Updated: 2023/03/06 04:36:34 by idaegyu          ###   ########.fr       */
+/*   Updated: 2023/03/06 04:45:00 by idaegyu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,6 @@ void	rot_upd_child_center(t_zip *zip)
 	if (zip->mdat->obj_type == CONE)
 		child_p = get_center(((t_cone *)zip->mdat->object)->bot, DISK);
 	else
-		child_p = get_center(((t_cylinder *)zip->mdat->object)->bot, DISK);
+		child_p = get_center(((t_cylinder *)zip->mdat->object)->top, DISK);
 	*child_p = vec_add(*center, vec_mul(*normal, *height));
 }
