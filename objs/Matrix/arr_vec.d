81c34aece908677be4c13a3fe238f127e26f8d54
이대규 <idaegyu@idaegyuui-MacBookAir.local>
2023-03-06 13:28:34 +0900

    start local_y rotate

---
+++ objs/Matrix/arr_vec.d
@@ -0,0 +1,9 @@
+objs/Matrix/arr_vec.o: srcs/Matrix/arr_vec.c srcs/Matrix/../math/math.h \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include/math.h \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include/sys/cdefs.h \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include/sys/_symbol_aliasing.h \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include/sys/_posix_availability.h \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include/Availability.h \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include/AvailabilityVersions.h \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include/AvailabilityInternal.h \
+  srcs/Matrix/./matrix.h srcs/Matrix/../vector/vector.h
